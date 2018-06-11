9959593fa9db66620cd1577039d10ab961aa1ec1
qiuyanjun <qyjwork@163.com>
2018-06-11 09:48:17 +0800

    master

--- flask_web/Views/Index.py
+++ flask_web/Views/Index.py
@@ -12,10 +12,7 @@ def hello_world():
 
 @view.route('/GetUsers/<userId>')
 def GetUsersByUserId(userId):
-    users = DB.Users.find({"U refusing to merge unrelated historiesserId": userId})
-    if users.count() ==0:
-        DB.Users.insert({"UserId":"5F38FF88D6DA4965B902016261F55A83","Age":20})
-        DB.Users.insert({"UserId":"2401A71D879E49F0BE68B63FA410EE7A","Age":30})
+    users = DB.Users.find({"UserId": userId})
     return render_template("index.html", users=users)
 
 
