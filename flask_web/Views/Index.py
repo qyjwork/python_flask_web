e69f9a5a0cfcbb1950b562aa33040fd8133e0fe5
qiuyanjun <qyjwork@163.com>
2018-06-12 16:50:25 +0800

    201806121650

--- flask_web/Views/Index.py
+++ flask_web/Views/Index.py
@@ -1,8 +1,7 @@
-import app
 from DataAccess import DB
-from flask import render_template, Blueprint
+from flask import render_template,session,Blueprint,Response,make_response,request,redirect,url_for,abort,flash
 
-view = Blueprint("Index", __name__) #, template_folder="templates"
+view = Blueprint("Index", __name__)  # , template_folder="templates"
 
 
 @view.route('/')
@@ -12,11 +11,50 @@ def hello_world():
 
 @view.route('/GetUsers/<userId>')
 def GetUsersByUserId(userId):
-    users = DB.Users.find({"UserId": userId})
-    return render_template("index.html", users=users)
+    #redirect
+    if userId =="1":
+        flash("this is message")
+        return redirect(url_for("Index.hello_world"))
+    #abort
+    elif userId=="2":
+        abort(500)
+    #render_template
+    else:
+        users = DB.Users.find({"UserId": userId})
+        return render_template("index.html", users=users)
 
 
 @view.route('/GetUsers')
 def GetUsers():
     users = DB.Users.find({})
     return render_template("index.html", users=users)
+
+
+@view.route('/setcookie/<userid>')
+def setcookie(userid):
+    users = DB.Users.find({})
+    res = make_response(render_template("index.html", users=users))
+    res.set_cookie('userID', userid)
+    return res
+
+
+
+@view.route('/getcookie')
+def getcookie():
+    userid = request.cookies.get('userID')
+    return render_template("hello.html",userid=userid)
+
+
+
+@view.route('/setsession/<userid>')
+def setsession(userid):
+    session["userId"] = userid
+    return render_template("hello.html",userid=userid)
+
+
+
+@view.route('/getsession')
+def getsession():
+    userid = session["userId"]
+    return render_template("hello.html",userid=userid)
+
--- flask_web/app.py
+++ flask_web/app.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 from Views import Index
-
 
 app = Flask(__name__,static_folder="/templates")
 app.register_blueprint(Index.view)
-
-
+app.secret_key ='my super secret key'.encode('utf8')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run( host='127.0.0.1', port=8000, debug=True )
